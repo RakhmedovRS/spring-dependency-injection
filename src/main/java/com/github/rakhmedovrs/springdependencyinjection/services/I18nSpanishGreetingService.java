dbbb2a42920e5e4a77b099daeedc6b36db679e3d
rassoll <rassoll32@gmail.com>
2020-05-02 21:33:54 +0300

    Default profile added

--- src/main/java/com/github/rakhmedovrs/springdependencyinjection/services/I18nSpanishGreetingService.java
+++ src/main/java/com/github/rakhmedovrs/springdependencyinjection/services/I18nSpanishGreetingService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
  * @author RakhmedovRS
  * @created 02-May-20
  */
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService
 {
